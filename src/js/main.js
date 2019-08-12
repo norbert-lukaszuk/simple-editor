1518239198a65ffd337cce1c8484ab5c84e092d5
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-08-12 16:17:47 +0200

    animation & transform

--- src/js/main.js
+++ src/js/main.js
@@ -30,10 +30,11 @@ const inputLS = document.querySelector('.textArea');
 const loadButton = document.querySelector('.buttons__load--js');
 const saveButton = document.querySelector('.buttons__save--js');
 const clearButton = document.querySelector('.buttons__clear--js');
+const resetButton = document.querySelector('.buttons__clearInput--js')
 
 // Nasłuchuje klikniecia load button i wypisuje przez '.innerHTML' pobrane z localStorage dane jeśli są zapiane
 loadButton.addEventListener('click', (e)=>{inputLS.innerHTML=(localStorage.getItem('inputTextArea'))});
-C
+
 saveButton.addEventListener('click',(e)=>{localStorage.setItem('inputTextArea',inputLS.value)});
 // Nasłuchuje kliknięcia w clear button po czym czyści zawartość localStorage metodą '.clear()'
 clearButton.addEventListener('click',(e)=>{localStorage.clear()});
