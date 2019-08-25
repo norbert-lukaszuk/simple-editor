1eec59e7bbfec6e1c6f72a9404217078dd439816
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-08-25 12:06:50 +0200

    feat: clear board

--- src/js/main.js
+++ src/js/main.js
@@ -20,19 +20,21 @@ if ('serviceWorker' in navigator) {
 const inputLS = document.querySelector('.textArea');
 
 
+
 const loadButton = document.querySelector('.buttons__load--js');
 const saveButton = document.querySelector('.buttons__save--js');
 const clearButton = document.querySelector('.buttons__clear--js');
 const resetButton = document.querySelector('.buttons__clearInput--js')
 
+//Nasłuchuje kliknięcia w reset button po czym czyści imput czyli textarea
+resetButton.addEventListener('click', (e)=>{inputLS.innerHTML="clear"});
 // Nasłuchuje klikniecia load button i wypisuje przez '.innerHTML' pobrane z localStorage dane jeśli są zapiane
 loadButton.addEventListener('click', (e)=>{inputLS.innerHTML=(localStorage.getItem('inputTextArea'))});
 
 saveButton.addEventListener('click',(e)=>{localStorage.setItem('inputTextArea',inputLS.value)});
 // Nasłuchuje kliknięcia w clear button po czym czyści zawartość localStorage metodą '.clear()'
 clearButton.addEventListener('click',(e)=>{localStorage.clear()});
-//Nasłuchuje kliknięcia w reset button po czym czyści imput czyli textarea
-resetButton.addEventListener('click', (e)=>{inputLS.innerHTML=""});
+
 
 
 
