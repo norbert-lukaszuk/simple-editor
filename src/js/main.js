ab9da01f083b71fd2c354f265f01349edd107113
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-08-27 08:45:42 +0200

    feat: clear textarea

--- src/js/main.js
+++ src/js/main.js
@@ -27,9 +27,9 @@ const clearButton = document.querySelector('.buttons__clear--js');
 const resetButton = document.querySelector('.buttons__clearInput--js')
 
 //Nasłuchuje kliknięcia w reset button po czym czyści imput czyli textarea
-resetButton.addEventListener('click', (e)=>{inputLS.innerHTML="clear"});
+resetButton.addEventListener('click', (e)=>{inputLS.value=""}, false);
 // Nasłuchuje klikniecia load button i wypisuje przez '.innerHTML' pobrane z localStorage dane jeśli są zapiane
-loadButton.addEventListener('click', (e)=>{inputLS.innerHTML=(localStorage.getItem('inputTextArea'))});
+loadButton.addEventListener('click', (e)=>{inputLS.value =(localStorage.getItem('inputTextArea'))});
 
 saveButton.addEventListener('click',(e)=>{localStorage.setItem('inputTextArea',inputLS.value)});
 // Nasłuchuje kliknięcia w clear button po czym czyści zawartość localStorage metodą '.clear()'
