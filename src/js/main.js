d1ce597a49f5ebcc59d15632a0b0fed46fdbcfc3
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-09-03 16:50:21 +0200

    feat: multiline save

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,16 @@
 
 // Zapisuje w zmiennej input wpisywany w textarea
 const inputLS = document.querySelector('.textArea');
-localStorage.setItem('counter', 0);
+if (localStorage.getItem('counter')== null)
+{
+  localStorage.setItem('counter', 0);
+}
+else{
+
+};
 let counter = localStorage.getItem('counter');
 myArray=new Array();
-console.log(myArray);
+
 
 const loadButton = document.querySelector('.buttons__load--js');
 const saveButton = document.querySelector('.buttons__save--js');
@@ -17,18 +23,29 @@ const resetButton = document.querySelector('.buttons__clearInput--js')
 //Nasłuchuje kliknięcia w reset button po czym czyści imput czyli textarea
 resetButton.addEventListener('click', (e)=>{inputLS.value=""}, false);
 // Nasłuchuje klikniecia load button i wypisuje przez '.innerHTML' pobrane z localStorage dane jeśli są zapiane
-loadButton.addEventListener('click', (e)=>{/*inputLS.value =(localStorage.getItem(`inputTextArea ${localStorage.getItem('counter')}`))*/
+loadButton.addEventListener('click', (e)=>{
+ 
+  localStorage.setItem('savedText',myArray);
+  inputLS.value =(localStorage.getItem('savedText'));
+  // console.log(counter);
+  // counter++;
   
-    let loader = localStorage.getItem('counter');
-    for(let i=loader; i<=0; i--){myArray.push(localStorage.getItem(`inputTextArea ${i}`))}
-  inputLS.value = myArray;
+  
+  //   let loader = localStorage.getItem('counter');
+  //   for(let i=loader; i<=0; i--){myArray.push(localStorage.getItem(`inputTextArea ${i}`))}
+  // inputLS.value = myArray;
 });
 
 
 saveButton.addEventListener('click',(e)=>{localStorage.setItem(`inputTextArea ${localStorage.getItem('counter')}`,inputLS.value);
-    counter = localStorage.getItem('counter');
+    let saveCount = localStorage.getItem('counter');
     counter++;
     localStorage.setItem('counter',counter);
+    let arrayElement = (localStorage.getItem(`inputTextArea ${saveCount}`));
+    console.log(arrayElement);
+    myArray.push(arrayElement);
+    console.log(myArray);
+    
 
 });
 // Nasłuchuje kliknięcia w clear button po czym czyści zawartość localStorage metodą '.clear()'
