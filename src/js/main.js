d72ae2a8556eac6993aa7e6a2fa77222ed5d2e8c
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-09-06 12:31:22 +0200

    feat: load multilines

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 
 // Zapisuje w zmiennej input wpisywany w textarea
 const inputLS = document.querySelector('.textArea');
+// Sprawdza czy licznik counter w localStorage ma jakąś wartość jeśli tak pozostawia ją jeśli nieokreśloną przypisuje 0
 if (localStorage.getItem('counter')== null)
 {
   localStorage.setItem('counter', 0);
@@ -11,7 +12,9 @@ if (localStorage.getItem('counter')== null)
 else{
 
 };
+// przypisuje do zmiennej couter wartość pobraną z loacalStorage
 let counter = localStorage.getItem('counter');
+// tworzy pustą tablicę do której zapiszemy wszystkie wprowadzone elemennty 
 myArray=new Array();
 
 
@@ -21,25 +24,17 @@ const clearButton = document.querySelector('.buttons__clear--js');
 const resetButton = document.querySelector('.buttons__clearInput--js')
 
 //Nasłuchuje kliknięcia w reset button po czym czyści imput czyli textarea
-resetButton.addEventListener('click', (e)=>{inputLS.value="";
-      
-}, false);
+resetButton.addEventListener('click', (e)=>{inputLS.value="";}, false);
 // Nasłuchuje klikniecia load button i wypisuje przez '.innerHTML' pobrane z localStorage dane jeśli są zapiane
 loadButton.addEventListener('click', (e)=>{
   for(i=0;i<=localStorage.getItem('counter');i++){
     myArray[i]=localStorage.getItem(`inputTextArea ${i}`);
   }
-  
-
-  localStorage.setItem('savedText',myArray);
+  // pobiera z tablicy poszczególne elementy i tworzy string gdzie poszczególne elementy tablicy są wypisywane w nowych liniach
+  myString=myArray.join('\n');
+  localStorage.setItem('savedText',myString);
   inputLS.value =(localStorage.getItem('savedText'));
-  // console.log(counter);
-  // counter++;
   
-  
-  //   let loader = localStorage.getItem('counter');
-  //   for(let i=loader; i<=0; i--){myArray.push(localStorage.getItem(`inputTextArea ${i}`))}
-  // inputLS.value = myArray;
 });
 
 
@@ -48,12 +43,10 @@ saveButton.addEventListener('click',(e)=>{localStorage.setItem(`inputTextArea ${
     counter++;
     localStorage.setItem('counter',counter);
     let arrayElement = (localStorage.getItem(`inputTextArea ${saveCount}`));
-    console.log(arrayElement);
     myArray.push(arrayElement);
-    console.log(myArray);
     
-
 });
+
 // Nasłuchuje kliknięcia w clear button po czym czyści zawartość localStorage metodą '.clear()'
 clearButton.addEventListener('click',(e)=>{localStorage.clear();
         localStorage.setItem('counter', 0);
